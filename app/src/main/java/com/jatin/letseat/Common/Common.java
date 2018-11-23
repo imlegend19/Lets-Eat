e22fc3f35c65094768bb0052bad86d899a598e9a
Jatin Kumar Jain <31545613+Jatzj7@users.noreply.github.com>
2018-11-23 21:07:54 +0530

    added

---
+++ app/src/main/java/com/jatin/letseat/Common/Common.java
@@ -0,0 +1,7 @@
+package com.jatin.letseat.Common;
+
+import com.jatin.letseat.model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/jatin/letseat/Interface/ItemClickListener.java
@@ -0,0 +1,5 @@
+package com.jatin.letseat.Interface;
+import android.view.View;
+public interface ItemClickListener {
+    void onClick(View view,int position,boolean isLongClick);
+}
